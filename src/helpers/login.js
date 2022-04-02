b611729fae8d7fd41500c60839e47d90c5744315
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-04-02 20:28:33 +0200

    add login functionality

--- src/helpers/login.js
+++ src/helpers/login.js
@@ -1,1 +1,8 @@
-"template";
+const ENDPOINT = `${process.env.REACT_APP_API_URL}`;
+
+export default function loger(sessionParams) {
+  return axios.post(`${ENDPOINT}/login`, sessionParams).then((res) => {
+    if (!res.data.token) throw new Error("Response is NOT ok");
+    return res.data;
+  });
+}
---
+++ src/helpers/useSession.js
@@ -0,0 +1,38 @@
+import { useCallback, useContext } from "react";
+import Context from "contexts/user";
+import loginSrv from "helpers/login";
+import { useNavigate } from "react-router-dom";
+
+export const useSession = () => {
+  const navigate = useNavigate();
+  const { jwt, setJWT, user, setUser } = useContext(Context);
+  let logfail = "alert inicial";
+  const loger = useCallback(
+    ({ login, password }) => {
+      loginSrv({ login, password })
+        .then(({ token, rol, login, id }) => {
+          window.sessionStorage.setItem("jwt", token);
+          window.sessionStorage.setItem("user", `${id}, ${login}, ${rol}`);
+          setJWT(token);
+          setUser(`${id}, ${login}, ${rol}`);
+        })
+        .catch((err) => {
+          logfail = true;
+          window.sessionStorage.removeItem("jwt");
+          window.sessionStorage.removeItem("user");
+          console.error(err);
+        });
+    },
+    [setJWT]
+  );
+
+  const logout = useCallback(() => {
+    window.sessionStorage.removeItem("jwt");
+    window.sessionStorage.removeItem("user");
+    setJWT(null);
+    setUser(null);
+    navigate("/login");
+  }, [setJWT]);
+
+  return { user, logout, loger, isLogged: Boolean(jwt), logfail };
+};
