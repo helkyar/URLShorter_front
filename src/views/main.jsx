9516b28c3f92ebc60dc652fb7bca025415861c61
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-04-17 23:44:37 +0200

    Add front form

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -1,15 +1,28 @@
-import { useSession } from "helpers/session/useSession";
-import React from "react";
-import { useNavigate } from "react-router";
+import Context from "contexts/user";
+import postUrl from "helpers/urls/postUrl";
+import React, { useContext, useState } from "react";
+// import { useSession } from "helpers/session/useSession";
+// import { useNavigate } from "react-router";
 
 export const Main = () => {
-  const navigate = useNavigate();
-  const { logout } = useSession();
+  // const navigate = useNavigate();
+  // const { logout } = useSession();
+  const { jwt } = useContext(Context)
+  const [text, setText] = useState("");
+  const handleSubmit = async () => {};
+    await postUrl({ url: text }, jwt);
   return (
     <div className="app">
-      <h1>Make History</h1>
+      {/* <h1>Make History</h1>
       <button onClick={() => navigate("/template")}>templates</button>
-      <button onClick={() => logout()}>logout</button>
+      <button onClick={() => logout()}>logout</button> */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
