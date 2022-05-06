07b326fcf14e45f16c5ab7e08de3454e3f5163b1
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-05-06 11:03:21 +0200

    Front styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>Shorty</h1>
+      <nav>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+      </nav>
+    </header>
+  );
+};
